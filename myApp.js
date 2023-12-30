5ef0cdc0ba140c9b3ec666495827b163718ae031
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2023-12-30 01:30:16 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,54 +1,110 @@
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+// 1) Install and Set Up Mongoose
+/** CONNECT TO THE MONGODB ATLAS DATABASE USING THE PROVIDED URI **/
+/* MONGO_URI=mongodb+srv://Username:Password@ServerOrCluster/DesiredDatabaseName?AdditionalConnectionOptions */
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-let Person;
+// 2) Create a "Person" Model
+/** ASSIGN MONGOOSE SCHEMA TO A VARIABLE **/
+const Schema = mongoose.Schema;
 
+/** CREATE PERSON SCHEMA **/
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: { type: [String], required: true },
+});
+
+/** CREATE PERSON MODEL FROM THE SCHEMA **/
+const Person = mongoose.model("Person", personSchema);
+
+// 3) Create and Save a Record of a Model
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const person = new Person({
+    name: "Kelly Cristina Moreira",
+    age: 19,
+    favoriteFoods: ["Strogonoff", "Rice and Beans"],
+  });
+
+  person.save((err, data) => (err ? done(null) : done(null, data)));
 };
 
+// 4) Create Many Records People with model.create()
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) =>
+    err ? done(err) : done(null, data),
+  );
 };
 
+// 5) Use model.find() to Search Your Database
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) =>
+    err ? done(err) : done(null, data),
+  );
 };
 
+// 6) Use model.findOne() to Return a Single Matching Document from Your Database
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) =>
+    err ? done(err) : done(null, data),
+  );
 };
 
+// 7) Use model.findById() to Search Your Database By _id
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, data) =>
+    err ? done(err) : done(null, data),
+  );
 };
 
+// 8) Perform Classic Updates by Running Find, Edit, then Save
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, data) => {
+    data.favoriteFoods.push(foodToAdd);
+    data.save((err, savedData) => done(err, savedData));
+  });
 };
 
+// 9) Perform New Updates on a Document Using model.findOneAndUpdate()
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { $set: { age: ageToSet } },
+    { new: true },
+    (err, data) => (err ? done(err) : done(null, data)),
+  );
 };
 
+// 10) Delete One Document Using model.findByIdAndRemove
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({ _id: personId }, (err, data) =>
+    err ? done(err) : done(null, data),
+  );
 };
 
+// 11) Delete Many Documents with model.remove()
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, data) =>
+    err ? done(err) : done(null, data),
+  );
 };
 
+// 12) Delete Many Documents with model.remove()
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: "asc" })
+    .limit(2)
+    .select("-age")
+    .exec((err, data) => done(err, data));
 };
 
 /** **Well Done !!**
